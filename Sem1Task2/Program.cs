af6c867a6926b1e957ab0ac3df7ff24b55fb4bc1
Darya Bugaets <40bugaets@mail.ru>
2023-02-14 23:07:48 +0300

    add Task1,2

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Вводим первое число
+Console.WriteLine("Введите число №1: ");
+int numA=Convert.ToInt32(Console.ReadLine());
+// Вводим второе число
+Console.WriteLine("Введите число №2: ");
+int numB=Convert.ToInt32(Console.ReadLine());
+// Сравниваем числа
+if(numA>numB)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
+
+
+
+
+
+// // Вводим первое число
+// Console.WriteLine("Введите число №1: ");
+// string? numLineA=Console.ReadLine;
+// // Вводим второе число
+// Console.WriteLine("Введите число №2: ");
+// string? numLineB=Console.ReadLine;
+
+// // проверяем на null
+// if(numLineA != null && numLineB != null)
+// {
+// // преобразуем наши числа
+//     int numA=int.Parse(numLineA);
+//     int numB=int.Parse(numLineB);
+//     if(numA>numB)
+//     {
+//         Console.WriteLine("Первое число больше второго");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Второе число больше первого");
+//     }
+// }
+
+
+// Вводим первое число
+Console.WriteLine("Введите число №1: ");
+int numA=Convert.ToInt32(Console.ReadLine());
+// Вводим второе число
+Console.WriteLine("Введите число №2: ");
+int numB=Convert.ToInt32(Console.ReadLine());
+// Сравниваем числа
+if(numA>numB)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
+
