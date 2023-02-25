fa60d69f9f342628f4c6d3b735b502b79a982d8a
Darya Bugaets <40bugaets@mail.ru>
2023-02-25 23:56:27 +0300

    Task17

---
+++ Sem3Task17/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №17
+// Напишите программу, которая принимает точки ввода координат (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти назад, в котором находится эта точка.
+
+// <тип> <имя метода>(<тип> <название устанавливает>)
+// {
+//      возвращаем <тип>
+// }
+
+// Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0"); 
+}
+// Метод определить четверть по координатам точки
+void  PrintQuterTest (int  x ,int  y )
+{
+    if( x  >  0  &&  y  >  0 ) Console.WriteLine ( " Точка в 1 четверти " );
+    if( x  >  0  &&  y  <  0 ) Console.WriteLine ( " Точка во 2 четверти " );
+    if( x  <  0  &&  y  <  0 ) Console.WriteLine ( " Точка в 3 четверти " );
+    if( x  <  0  &&  y  >  0 ) Console.WriteLine ( " Точка в 4 четверти " );
+}
+int  coordX  =  ReadData( " Введите координату X: ");
+int  coordY  =  ReadData( " Введите координату Y: ");
+PrintQuterTest(coordX ,coordY );
